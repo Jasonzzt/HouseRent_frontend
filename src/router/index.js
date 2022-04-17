ad5a55fbd336a2fc7e0c84aad79143b789373efe
Jasonzzt <1273302566@qq.com>
2022-04-17 11:46:46 +0800

    plus index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Login from '../views/Login.vue'
 import Register from  '../views/Register.vue'
-
+import Index from '../views/Index.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -22,7 +22,12 @@ const routes = [
     path:'/register',
     name:'register',
     component: Register
-  }
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
 ]
 
 const router = new VueRouter({
