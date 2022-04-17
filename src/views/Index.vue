9858399d16a098540f2fe0b34a943038f73334fa
Jasonzzt <1273302566@qq.com>
2022-04-17 13:33:00 +0800

    change index.vue 1

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,9 +7,9 @@
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
-    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-menu-item index="1">房源信息</el-menu-item>
     <el-submenu index="2">
-      <template slot="title">我的工作台</template>
+      <template slot="title">我的订单</template>
       <el-menu-item index="2-1">选项1</el-menu-item>
       <el-menu-item index="2-2">选项2</el-menu-item>
       <el-menu-item index="2-3">选项3</el-menu-item>
@@ -20,8 +20,8 @@
         <el-menu-item index="2-4-3">选项3</el-menu-item>
       </el-submenu>
     </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    <el-menu-item index="3" >消息中心</el-menu-item>
+    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">发布房源</a></el-menu-item>
   </el-menu>
 
 </template>
