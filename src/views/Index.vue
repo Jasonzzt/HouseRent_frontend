e98802ef050583f82959243e6f7a63b5a98d3d7e
Jasonzzt <1273302566@qq.com>
2022-04-25 16:36:00 +0800

    change foot

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,7 +7,27 @@
       <el-main>
         <router-view></router-view>
       </el-main>
-      <el-footer>Foot</el-footer>
+      <el-footer>
+        <el-button type="text" @click="dialogVisible = true">免责声明</el-button>
+        <br>
+        <span>
+        © 2022 711201,SEU
+        </span>
+        <el-dialog
+            title="住房租赁平台免责声明"
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+          <span>1.平台对于数据的真实性不负责，请用户提高警惕。</span>
+          <br>
+          <span>2.平台对于交易无任何干涉，请尽量线下交易。</span>
+          <br>
+          <span>3.平台对于用户信息数据将做到严格保密。</span>
+          <span slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="dialogVisible = false">确认知悉</el-button>
+  </span>
+        </el-dialog>
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -19,14 +39,18 @@ export default {
   name: "index",
   components: {
     NaviMenu
+  },
+  data(){
+    return {
+      dialogVisible: false
+    };
   }
 };
 </script>
 
 <style scoped>
-.el-header,
 .el-footer {
-  background-color: #b3c0d1;
+  background-color: #dbe0e7;
   color: #333;
   text-align: center;
   height: 100px;
